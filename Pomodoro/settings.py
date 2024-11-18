39a4fafccef9a1fa0f2f3e48f4307540e87bdacd
Me <newprovkov@yandex.ru>
2024-11-18 18:45:16 +0300

    fix database&cache access

--- Pomodoro/settings.py
+++ Pomodoro/settings.py
@@ -5,9 +5,12 @@ class Settings(BaseSettings):
     DB_PORT: int = 5432
     DB_USER: str = 'postgres'
     DB_PASSWORD: str = '1234'
+    DB_DRIVER: str = 'postgresql+psycopg'
     DB_NAME: str = 'pomodoro'
     CACHE_HOST: str = '0.0.0.0'
     CACHE_PORT: int = 6379
     CACHE_DB: int = 0
     
-    
+    @property
+    def db_url(self):
+        return f'{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@P{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
